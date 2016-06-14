73eb6342296b0c6765718ccd23a66c0ad00e9f14
LBezerril <bezerril.leandro@gmail.com>
2016-06-14 09:46:21 -0300

    cgiwork_plain

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 # Verificar dependencias e copiar o script para um dos diretorios do PATH
 Permissao: -rwxr-xr-x 1 root root --- /usr/bin/cgiwork
+
+cgiwork >> /usr/local/bin
+cgiwork_$type >> /opt/cgiwork/scripts
