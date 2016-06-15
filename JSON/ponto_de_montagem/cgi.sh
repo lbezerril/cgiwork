d55e87365f4c29e40e01096bc05b7d04e635f994
LBezerril <bezerril.leandro@gmail.com>
2016-06-15 16:44:49 -0300

    JSON implementation - TODO

--- JSON/ponto_de_montagem/cgi.sh
+++ JSON/ponto_de_montagem/cgi.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
 ARRAY=( "$@" );
-first_iteration=true;
+FIRST_ITERATION=0; #0=true;1=false
+
+function is_first_iteration() {
+	return $FIRST_ITERATION;
+}
+
+function fix_null() {
+	# $1: index of ARRAY
+	if [ "$(echo "${ARRAY[$1]}" | cut -d'=' -f2- | sed 's/ //g')" == "" ]; then
+		ARRAY[$1]="$(echo "${ARRAY[$1]}" | cut -d'=' -f-1)=null";
+	fi;
+}
 
 function get_attributes() {
+	# $1: class name
 	for i in "${!ARRAY[@]}"; do
-		if [ ! -z $first_iteration ]; then
-			if [ "$(echo "${ARRAY[$i]}" | cut -d'=' -f2- | sed 's/ //g')" == "" ]; then
-				ARRAY[$i]="$(echo "${ARRAY[$i]}" | cut -d'=' -f-1)=null";
-			fi;
-		fi;
+		if is_first_iteration; then fix_null $i; fi;
 		echo "${ARRAY[$i]}";
 	done |
-	grep -o "^$1[^.]*..\|^$1[^.=]*[=].*" |
-	#grep -o "^$1[.][^.]*..\|^$1[.][^.=]*[=].*" |
+
+	grep -Eo "^$1[^.]*..|^$1[^.=]*[=].*" |
+	#(^$1[.][^.=]*..)
+	#(^$1[.]([^.=]*..|[^.=]*=.*)) # Esse aqui
+
+	#grep -Eo "^$1[.][^.]*..|^$1[.][^.=]*[=].*" |
 	sed '/=/! { s/\(.*\)\.\([a-Z]\)$/\1.X/;s/\(.*\)\.\([0-9]\)$/\1.0/; }' |
 	sort -u;
 
-	unset first_iteration;
+	FIRST_ITERATION=1;
 }
 
 function attribute_is_value() {
+	# $1: attribute
 	echo "$1" | grep -q "=";
 }
 
 function attribute_is_object_array() {
+	# $1: attribute
 	return 1;
 }
 
 function attribute_is_object() {
+	# $1: attribute
 	return 1;
 }
 
 function attribute_is_value_array() {
+	# $1: attribute
 	return 1;
 }
 
@@ -41,20 +57,20 @@ function write_value() {
 	echo "\"$(echo "$1" | cut -d'=' -f1 | rev | cut -d'.' -f1 | rev)\": $(echo "$1" | cut -d'=' -f2-),";
 	# Remove element from array
 }
-function iterate_attributes() {
-	echo "$(get_attributes $1)" | while read line; do
-		if attribute_is_value "$line"; then write_value "$line"; continue; fi;
+
+function generate_class() {
+	# $1: class name
+	echo "{";
+	# For each attribute of class
+	echo "$(get_attributes $1)" | while read attribute; do
+		if attribute_is_value "$attribute"; then write_value "$attribute"; continue; fi;
 		#if attribute_is_value "$i"; then write_value "$i"; continue; fi;
 		#if attribute_is_value "$i"; then write_value "$i"; continue; fi;
 		#if attribute_is_value "$i"; then write_value "$i"; continue; fi;
 	done;
-}
-
-function generate_class() {
-	echo "{";
-	iterate_attributes "$1";
 	#echo -n "something,"; echo -e "\b}";
 	echo "}";
 }
 
 generate_class "applications.git.";
+#generate_class
