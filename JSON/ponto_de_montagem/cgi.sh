6154b17c9d7b224039efce1f0f8fd4f6e32d6ccb
LBezerril <bezerril.leandro@gmail.com>
2016-06-16 13:55:13 -0300

    Generate object from class

--- JSON/ponto_de_montagem/cgi.sh
+++ JSON/ponto_de_montagem/cgi.sh
@@ -42,7 +42,7 @@ function attribute_is_object_array() {
 
 function attribute_is_object() {
 	# $1: attribute
-	return 1;
+	echo "$1" | grep -q "X$";
 }
 
 function attribute_is_value_array() {
@@ -59,14 +59,29 @@ function write_value() {
 	unset ARRAY[$index]; # Remove element from array
 }
 
+function get_class_by_attribute() {
+	# $1: attribute
+	echo "$1" | grep -o '[.].*[.]' | sed 's/.$//g';
+}
+
+function print_opening_bracket() {
+	# $1: class annotation
+	if [[ "$1" == "" ]]; then
+		echo "{";
+	else
+		local class_name="$(echo "$1" | grep -Eo '[^.]*$')";
+		echo "\"$class_name\": {";
+	fi;
+}
+
 function generate_class() {
-	# $1: class name
-	echo "{";
+	# $1: class annotation
+	print_opening_bracket "$1";
 	# For each attribute of class
 	echo "$(get_attributes $1)" | while read attribute; do
 		if attribute_is_value "$attribute"; then write_value "$attribute"; continue; fi;
 		#if attribute_is_object_array "$attribute"; then write_value "$attribute"; continue; fi;
-		#if attribute_is_object "$attribute"; then write_value "$attribute"; continue; fi;
+		if attribute_is_object "$attribute"; then generate_class $(get_class_by_attribute "$attribute"); continue; fi;
 		#if attribute_is_value_array "$attribute"; then write_value "$attribute"; continue; fi;
 	done;
 	#echo -n "something,"; echo -e "\b}";
