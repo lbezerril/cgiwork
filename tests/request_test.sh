09c460edbb3fb3934ecbd2162dc31852482a0009
LBezerril <bezerril.leandro@gmail.com>
2016-10-14 21:44:08 -0300

    Remoção do cat - pelo laço while com read para imprimir stdout vindo da pipe e adição de um teste unitario

--- tests/request_test.sh
+++ tests/request_test.sh
@@ -15,54 +15,58 @@ export PATH="$PATH:$cgiwork_path:$assert_path"
 
 
 
-# Testa se as funções do módulo foram carregas
+# Testa se as funções do módulo foram carregas.
 __assert_equals_exit_status 0 type __get_method
 __assert_equals_exit_status 0 type __get_uri
 __assert_equals_exit_status 0 type __get_protocol
 __assert_equals_exit_status 0 type __get_parameters
 
-# Testa se as funções internas do módulo não foram carregas
+# Testa se as funções internas do módulo não foram carregas.
 # TODO: diferente de 0 (dash retorna 127)
 __assert_equals_exit_status 1 type __get_content_type
 __assert_equals_exit_status 1 type __get_content_length
 __assert_equals_exit_status 1 type __get_header_by_var
 
-# Testes da função __get_method
+# Testes da função __get_method.
 get_method_test() {
   __assert_equals_string "" "$(__get_method)"
   __assert_equals_exit_status 1 __get_method
 
   local REQUEST_METHOD=POST
+
   __assert_equals_string "POST" "$(__get_method)"
   __assert_equals_exit_status 0 __get_method
 }
 
-# Testes da função __get_uri
+# Testes da função __get_uri.
 get_uri_test() {
   __assert_equals_string "" "$(__get_uri)"
   __assert_equals_exit_status 1 __get_uri
 
   local REQUEST_URI=/
+
   __assert_equals_string "/" "$(__get_uri)"
   __assert_equals_exit_status 0 __get_uri
 }
 
-# Testes da função __get_protocol
+# Testes da função __get_protocol.
 get_protocol_test() {
   __assert_equals_string "" "$(__get_protocol)"
   __assert_equals_exit_status 1 __get_protocol
 
   local SERVER_PROTOCOL=HTTP/1.1
+
   __assert_equals_string "HTTP/1.1" "$(__get_protocol)"
   __assert_equals_exit_status 0 __get_protocol
 }
 
-# Testes da função __get_parameters passando argumento
+# Testes da função __get_parameters passando argumento.
 get_parameters_with_args_test() {
   __assert_equals_string "" "$(__get_parameters id)"
   __assert_equals_exit_status 1 __get_parameters id
 
   local QUERY_STRING="id=1&product=2+2"
+
   __assert_equals_string "1" "$(__get_parameters id)"
   __assert_equals_exit_status 0 __get_parameters id
   __assert_equals_string "2 2" "$(__get_parameters product)"
@@ -71,11 +75,16 @@ get_parameters_with_args_test() {
   __assert_equals_exit_status 1 __get_parameters name
 }
 
-# Testes da função __get_parameters sem passar argumento
+# Testes da função __get_parameters sem passar argumento.
 get_parameters_without_args_test() {
+  # $@: Lista dos pares key=value de parâmetro esperado.
+
   __assert_equals_string "" "$(__get_parameters)"
   __assert_equals_exit_status 1 __get_parameters
+
   local QUERY_STRING="id=1&product=2+2"
+  __assert_equals_exit_status 0 __get_parameters
+
   __get_parameters | while read parameter; do
     __assert_equals_string "$1" "$parameter"
     shift
